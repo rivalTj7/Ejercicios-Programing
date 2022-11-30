61fce7e3fae0d5590277fec9762c49cb51d64291
rivalTj <rival.alex7@gmail.com>
2022-11-29 21:39:22 -0600

    segundo ejemplo

---
+++ EstudiantesOnline/Conexiones.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Web
+Imports System.Data.SqlClient
+Public Class Conexiones
+    Public Shared Cnn As SqlClient.SqlConnection
+    Public Shared Validar As String = "0"
+
+    Public Shared Sub AbrirConexion()
+        Cnn = New SqlConnection("Server=localhost\SQLEXPRESS;Database=escuela;User Id=julio;Password=ciencia123;")
+
+    End Sub
+
+
+End Class
